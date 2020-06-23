0393ee3ad46f9e26abeffe75b4f58745bfa088b3
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-06-23 16:23:05 +0300

    minor changes in grid file

--- game-of-life/src/logic/UniverseGrid.js
+++ game-of-life/src/logic/UniverseGrid.js
@@ -1,23 +1,22 @@
-import React, {Components} from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-width: 25px;
-height: 25px;
-border: 2px solid black;
-background-color:${props => props.status ? "red" : "grey"};
+    width: 22px;
+    height: 22px;
+    border: 2px solid black;
+    background-color: ${props => props.status ? "red": "grey"};
 `;
-
 class UniverseGrid extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-
         }
     }
-    render(){
-        return(
-            <Container status={this.props.status} id="cell" onClick={()=> this.props.toggleCell(this.props.id)}></Container>
+    render() {
+        return (
+            <Container status={this.props.status} id='cell' onClick={() => this.props.toggleCell(this.props.id)}>
+            </Container>
         )
     }
 }
