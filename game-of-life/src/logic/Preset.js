04897c4c25708702bfeea598181aec6dff25515d
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-06-23 16:21:34 +0300

    change name files

---
+++ game-of-life/src/logic/Preset.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Preset=()=>{
+    return <div></div>
+};
+export default Preset;
--- game-of-life/src/logic/UniversePreset.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const UniversePreset=()=>{
-    return <div></div>
-};
-export default UniversePreset;
