c8cee8343b3e9066b2c540c26ec1b8eb5f394edf
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-06-23 20:18:07 +0300

    refactor code file

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import './App.css';
-import styled from 'styled-components';
-import Universe from './logic/Universe';
+import React, { Component } from "react";
+import "./App.css";
+
+import styled from "styled-components";
+import Universe from "./logic/Universe";
 
 const Title = styled.h1`
   width: 100%;
@@ -17,61 +18,85 @@ const Content = styled.div`
 `;
 
 const PresetsContainer = styled.div`
-  width: 300px;
-  border: 3px solid red;
+  width: 400px;
+  height: 300px;
+  margin: 80px;
+  border: 1px solid red;
 `;
 
-const Rules = styled.div`
-  width: 300px;
-  font-weight: bold;
-  border: 3px solid yellow;
+const Bullet = styled.li`
+  margin-bottom: 10px;
+  text-align: left;
 `;
-const Button = styled.button`
-    height: 25px;
-    font-size: 20px;
-    border-radius: 15px;
-    border: 2px solid black
-`;
+
 const AboutSection = styled.div`
+  font-size: 18px;
+  font-style: 2px italic solid black;
+  height: 190px;
   width: 100%;
 `;
+
+const Button = styled.button`
+  height: 25px;
+  font-size: 20px;
+  border-radius: 15px;
+  border: 1px solid black;
+`;
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      preset: null
-    }
+      preset: null,
+    };
   }
 
-  initialPreset = event => {
-    this.setState({preset: event.target.value});
-  }
+  initialPreset = (event) => {
+    this.setState({ preset: event.target.value });
+  };
 
   render() {
-
-
     return (
       <div className="App">
         <Content>
-        <Title>Conway's Game of Life</Title>
-        <AboutSection>
+          <Title>Conway's Game of Life</Title>
+          <AboutSection>
             <h2>About this Algorithm:</h2>
-            <p style={{ color: "black", size: "2px solid black" }}>The universe of the Game of Life is an infinite, two-dimensional orthogonal grid of square cells, each of which is in one of two
-              possible states, alive or dead. Every cell interacts with its eight neighbors, which are the cells that are horizontally, vertically, or diagonally adjacent.</p>
-            <p>If you would like to learn more about Conway's Game of Life, check out this <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">link!</a></p>
+            <p style={{ color: "black", size: "2px solid black" }}>
+              The universe of the Game of Life is an infinite, two-dimensional
+              orthogonal grid of square cells, each of which is in one of two
+              possible states, alive or dead. Every cell interacts with its
+              eight neighbors, which are the cells that are horizontally,
+              vertically, or diagonally adjacent.
+            </p>
+            <p>
+              If you would like to learn more about Conway's Game of Life, check
+              out this{" "}
+              <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">
+                link!
+              </a>
+            </p>
           </AboutSection>
-          <Universe preset={this.state.preset}/>
+          <Universe preset={this.state.preset} />
           <PresetsContainer>
-            <Button onClick={this.initialPreset} value='1'>Preset 1</Button>
-            <Button onClick={this.initialPreset} value='2'>Preset 2</Button>
-          </PresetsContainer>
-          <Rules className="Rules">
+            <Button onClick={this.initialPreset} value="1">
+              Preset 1
+            </Button>
+            <Button onClick={this.initialPreset} value="2">
+              Preset 2
+            </Button>
             <h2>Rules:</h2>
             <ul>
-              <li>If the cell is alive and has 2 or 3 neighbors, then it remains alive. Else it dies.</li>
-              <li>If the cell is dead and has exactly 3 neighbors, then it comes to life. Else if remains dead.</li>
+              <Bullet>
+                Any live cell with fewer than two live neighbors dies, as if by
+                under population.
+              </Bullet>
+              <Bullet>
+                Any live cell with two or three live neighbors lives on to the
+                next generation.
+              </Bullet>
             </ul>
-          </Rules>
+          </PresetsContainer>
         </Content>
       </div>
     );
